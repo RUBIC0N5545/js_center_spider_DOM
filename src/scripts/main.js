a9275aa83034d1232ea58fc30b29b14f6318a326
Dmytro <dmytrokovaliak@gmail.com>
2025-01-18 11:48:32 +0100

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,8 +7,8 @@ const fieldWidth = field.clientWidth;
 const fieldHeight = field.clientHeight;
 const spiderWidth = spider.clientWidth;
 const spiderHeight = spider.clientHeight;
-const topPos = (fieldWidth - spiderWidth) / 2;
-const leftPos = (fieldHeight - spiderHeight) / 2;
+const topPos = (fieldHeight - spiderHeight) / 2;
+const leftPos = (fieldWidth - spiderWidth) / 2;
 
 spider.style.top = topPos + 'px';
 spider.style.left = leftPos + 'px';
