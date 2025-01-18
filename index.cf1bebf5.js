e402512994cfe39dfc0cd4ad3f0c2945c42469ab
Dmytro <dmytrokovaliak@gmail.com>
2025-01-18 11:45:15 +0100

    make build

---
+++ index.cf1bebf5.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,i=e.clientHeight,c=t.clientWidth,n=t.clientHeight;t.style.top=(l-c)/2+"px",t.style.left=(i-n)/2+"px";
+//# sourceMappingURL=index.cf1bebf5.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const fieldWidth = field.clientWidth;
+const fieldHeight = field.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const topPos = (fieldWidth - spiderWidth) / 2;
+const leftPos = (fieldHeight - spiderHeight) / 2;
+spider.style.top = topPos + "px";
+spider.style.left = leftPos + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
